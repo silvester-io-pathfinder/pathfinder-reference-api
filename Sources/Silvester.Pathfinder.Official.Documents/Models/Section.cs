3ab78773b4942ba65551a9108815474f9d4b0a79
teun_ <teun_@DESKTOP-81EBD61>
2021-07-01 18:04:33 +0200

    Document builder structure.

---
+++ Sources/Silvester.Pathfinder.Official.Documents/Models/Section.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Silvester.Pathfinder.Official.Documents.Models
+{
+    public record Section
+    {
+        public string Title { get; }
+        public IReadOnlyList<SubSection> SubSections { get; }
+
+        public Section(string title, List<SubSection>? subSections = null)
+        {
+            Title = title;
+            SubSections = subSections ?? new List<SubSection>();
+        }
+
+        public class Builder
+        {
+            private string Title { get; }
+
+            private List<SubSection.Builder> SubSections { get; }
+
+            public Builder(string title)
+            {
+                Title = title;
+                SubSections = new List<SubSection.Builder>();
+            }
+
+            public Builder AddSubSection(string? title, Action<SubSection.Builder> buildAction)
+            {
+                SubSection.Builder builder = new SubSection.Builder(title);
+                SubSections.Add(builder);
+                buildAction(builder);
+
+                return this;
+            }
+
+            public Section Build()
+            {
+                return new Section(Title, SubSections.Select(e => e.Build()).ToList());
+            }
+        }
+    }
+}
