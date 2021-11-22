f89f4e71eb54aed05e71fc7d325afa0286721a77
Teun Kooijman <teun_@DESKTOP-81EBD61>
2021-11-22 19:47:42 +0100

    Added ToMarkdownLink

--- Sources/Seeding/Silvester.Pathfinder.Reference.Database.Seeding/EntityTemplate.cs
+++ Sources/Seeding/Silvester.Pathfinder.Reference.Database.Seeding/EntityTemplate.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Silvester.Pathfinder.Reference.Database.Extensions;
 using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Silvester.Pathfinder.Reference.Database.Seeding
@@ -22,5 +23,18 @@ namespace Silvester.Pathfinder.Reference.Database.Seeding
         }
 
         protected abstract TEntity GetEntity(ISeedBuilder builder);
+
+        protected string ToMarkdownLink<TReference>(string displayText, Guid entityId)
+        {
+            return $"[{displayText}]({GetCollectionName<TReference>()}/{entityId.ToString("d")})";
+        }
+
+        private string GetCollectionName<TReference>()
+        {
+            return typeof(ReferenceDatabase)
+                .GetProperties()
+                .First(e => e.PropertyType == typeof(DbSet<>).MakeGenericType(typeof(Models.Entities.Spell)))
+                .Name;
+        }
     }
 }
